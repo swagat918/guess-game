13c2bd6e740943a37273369d6731d9532ea8a58e
swagat918 <sigdelswagat918@gmail.com>
2025-10-08 22:18:41 +0545

    Add files via upload

---
+++ guess-game/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('guess-game').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
